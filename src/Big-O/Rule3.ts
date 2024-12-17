4c9dc6b507389aa2dde08743ac26fadb3aa8c985
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-12-17 19:28:59 +0330

    Rule 3

---
+++ src/Big-O/Rule3.ts
@@ -0,0 +1,14 @@
+// Rule 3 : Different terms for inputs
+
+function comparessBoxesTwice(boxes: string[] , boxes2: string[]){
+   boxes.forEach(function(boxes){
+      console.log(boxes);
+   })
+
+
+   boxes2.forEach(function(boxes){
+      console.log(boxes);
+   });
+}
+
+// O(a + b);
