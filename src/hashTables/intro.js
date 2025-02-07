3a52d0be959bd865fad3085ecc7baffb6ba0c468
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2025-02-07 15:25:04 +0330

    object and map

---
+++ src/hashTables/intro.js
@@ -0,0 +1,34 @@
+//object
+let phoneBook = {
+   "Alice": "09123456789",
+   "Bob": "09234567890",
+   "Charlie": "09345678901"
+};
+
+phoneBook["David"] = "09456789012";
+console.log(phoneBook["David"]);
+
+delete phoneBook["Charlie"];
+console.log(phoneBook["Charlie"]); 
+
+
+
+
+
+
+
+
+//Map
+// let phoneBook = new Map();
+
+// phoneBook.set("Rastin" , "09366793560");
+// phoneBook.set("jackie boy" , "09115461411");
+// phoneBook.set("Charlie", "09345678901");
+
+
+
+// console.log(phoneBook.get("Rastin"));
+// console.log(phoneBook.has("jackie boy"));
+
+// phoneBook.delete("Charlie");
+// console.log(phoneBook.has("Charlie"));
