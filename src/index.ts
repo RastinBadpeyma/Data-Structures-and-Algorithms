7ec377eb6fd2c5c7381c83adf677b6f4d2e9b07e
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2025-04-30 16:02:04 +0330

    node-modules

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+var objec1 = {value: 10};
+var objec2 = objec1;
+var objec3 = {value: 10};
+
+if (objec1 === objec2) {
+   console.log("objec1 == object2"); 
+}
+if(objec1 !== objec3){
+   console.log('object1 !== object3');
+}
+
